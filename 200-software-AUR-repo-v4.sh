e684d138fe8960ff8275646351efaa4643b36563
tvko <go.toba@gmail.com>
2019-10-22 19:10:08 +0300

    tvko soft packages

--- 200-software-AUR-repo-v4.sh
+++ 200-software-AUR-repo-v4.sh
@@ -18,13 +18,42 @@
 
 installed_dir=$(dirname $(readlink -f $(basename `pwd`)))
 
+sh $installed_dir/AUR/install-cmatrix-git-v*.sh
+sh $installed_dir/AUR/install-font-manager-v*.sh
 sh $installed_dir/AUR/install-gitfiend-v*.sh
-sh $installed_dir/AUR/install-insync-v*.sh
-sh $installed_dir/AUR/install-openbox-themes-pambudi-git-v*.sh
-#sh $installed_dir/AUR/install-mpd-ncmpcpp-v*.sh
-#sh $installed_dir/AUR/install-radiotray-v*.sh
-sh $installed_dir/AUR/install-virtualbox-for-linux-v*.sh
-sh $installed_dir/AUR/install-vivaldi-widevine-v*.sh
+sh $installed_dir/AUR/install-neofetch-v*.sh
+sh $installed_dir/AUR/install-numix-circle-icon-theme-git-v*.sh
+sh $installed_dir/AUR/install-numix-folders-git-v*.sh
+sh $installed_dir/AUR/install-numix-kde-theme-git-v*.sh
+sh $installed_dir/AUR/install-pamac-aur-v*.sh
+sh $installed_dir/AUR/install-sardi-extra-icons-v*.sh
+sh $installed_dir/AUR/install-sardi-icons-v*.sh
+sh $installed_dir/AUR/install-xcursor-breeze-v*.sh
+sh $installed_dir/AUR/install-earlyoom-v*.sh
+sh $installed_dir/AUR/install-mhwd-v*.sh
+sh $installed_dir/AUR/install-mhwd-db-v*.sh
+sh $installed_dir/AUR/install-pkgtools-v*.sh
+sh $installed_dir/AUR/install-antigen-git-v*.sh
+sh $installed_dir/AUR/install-nord-xfce-terminal-v*.sh
+sh $installed_dir/AUR/install-otf-inconsolata-powerline-git-v*.sh
+sh $installed_dir/AUR/install-otf-nerd-fonts-fira-code-v*.sh
+sh $installed_dir/AUR/install-hunspell-en-v*.sh
+sh $installed_dir/AUR/install-hunspell-ru-v*.sh
+sh $installed_dir/AUR/install-hyphen-ru-v*.sh
+sh $installed_dir/AUR/install-ngrok-v*.sh
+sh $installed_dir/AUR/install-wrk-v*.sh
+sh $installed_dir/AUR/install-asdf-vm-v*.sh
+sh $installed_dir/AUR/install-vundle-git-v*.sh
+sh $installed_dir/AUR/install-notion-app-v*.sh
+sh $installed_dir/AUR/install-postman-bin-v*.sh
+sh $installed_dir/AUR/install-smartgit-v*.sh
+sh $installed_dir/AUR/install-smartsynchronize-v*.sh
+sh $installed_dir/AUR/install-chromedriver-v*.sh
+sh $installed_dir/AUR/install-datagrip-v*.sh
+sh $installed_dir/AUR/install-datagrip-jre-v*.sh
+sh $installed_dir/AUR/install-intellij-idea-ultimate-edition-v*.sh
+sh $installed_dir/AUR/install-intellij-idea-ultimate-edition-jre-v*.sh
+
 
 # these come last always
 echo "Checking if icons from applications have a hardcoded path"
